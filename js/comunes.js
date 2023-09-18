4796b9d360ed347eeb0044ed2cf3343dec5831a5
leo-con <56977073+leo-con@users.noreply.github.com>
2023-09-18 17:56:03 -0400

    inicial

---
+++ js/comunes.js
@@ -0,0 +1,36 @@
+$(document).ready(function () {
+  genesysWidget();
+});
+
+var intervalID;
+function genesysWidget() {
+  (function (g, e, n, es, ys) {
+    g["_genesysJs"] = e;
+    g[e] =
+      g[e] ||
+      function () {
+        (g[e].q = g[e].q || []).push(arguments);
+      };
+    g[e].t = 1 * new Date();
+    g[e].c = es;
+    ys = document.createElement("script");
+    ys.async = 1;
+    ys.src = n;
+    ys.charset = "utf-8";
+    document.head.appendChild(ys);
+  })(window, "Genesys", "https://apps.mypurecloud.com/genesys-bootstrap/genesys.min.js", {
+    environment: "prod",
+    deploymentId: "4c108308-0722-4ebe-b89d-c91c01388dd2",
+  });
+}
+
+function ToggleWidget() {
+  var boton = document.getElementById("btnChat");
+  if (boton.innerHTML == "Iniciar Chat") {
+    Genesys("command", "Messenger.open", {}, function (o) {});
+    boton.innerHTML = "Ocultar Chat";
+  } else {
+    Genesys("command", "Messenger.close", {}, function (o) {});
+    boton.innerHTML = "Iniciar Chat";
+  }
+}
